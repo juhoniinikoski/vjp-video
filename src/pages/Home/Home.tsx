c9f802feb5ca78626335c2fe95a3154d597b9283
juhoniinikoski <juho.niinikoski@aalto.fi>
2022-04-24 19:44:33 +0300

    finished

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import './Home.css';
 import video from '../../utils/content/VJPvideo.mp4'
+import thumbnail from '../../utils/content/thumbnail.png'
 
 const Home = () => {
   return (
@@ -12,7 +13,7 @@ const Home = () => {
 
 const Embed = () => (
   <div className="video-responsive">
-    <video controls>
+    <video controls poster={thumbnail}>
       <source src={video} type="video/mp4"/>
     </video>
   </div>
