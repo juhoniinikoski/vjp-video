5f73faf5ff1cc6af9e8d5a6c36ce9ea5c98080d0
juhoniinikoski <juho.niinikoski@aalto.fi>
2022-04-24 13:00:51 +0300

    real video

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,30 +1,16 @@
 import * as React from 'react';
 import './Home.css';
-import video from '../../utils/content/video-3.mp4'
+import video from '../../utils/content/VJPvideo.mp4'
 
 const Home = () => {
   return (
     <div className='main-container'>
-      <YoutubeEmbed2 embedId="testi"/>
+      <Embed/>
     </div>
   );
 };
 
-const YoutubeEmbed = ({embedId}: {embedId: string}) => (
-  <div className="video-responsive">
-    <iframe
-      width="853"
-      height="480"
-      src={`https://www.youtube.com/embed/${embedId}`}
-      frameBorder="0"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-      allowFullScreen
-      title="Embedded youtube"
-    />
-  </div>
-);
-
-const YoutubeEmbed2 = ({embedId}: {embedId: string}) => (
+const Embed = () => (
   <div className="video-responsive">
     <video controls>
       <source src={video} type="video/mp4"/>
